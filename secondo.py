a9aff71b49f0f4549d62f84b4d44652452dc8b2e
elmoriconi <159887303+elmoriconi@users.noreply.github.com>
2024-02-13 22:14:45 +0100

    3 update secondo.py

--- secondo.py
+++ secondo.py
@@ -2,11 +2,31 @@ from math import sqrt
 
 print("ipotenusa = " sqrt((10,123 ** 2) + (30,456 ** ")))
                                            
-# in alternativa ho provato
-
+# in alternativa ho provato 
+                                           
+from math import sqrt
+                                           
 float c = 0
 a = 10,123 ** 2
 b = 30,456 ** 2
 c = a + b
 
 print(sqrt(c))
+
+# quello che dice chatgpt
+import math
+
+def calcola_ipotenusa(cateto1, cateto2):
+  ipotenusa = math.sqrt(cateto1*2 + cateto2*2)
+  return ipotenusa
+
+cateto1 = float(input("inserisci la lunghezza del primo cateto: "))
+cateto2 = float(input("inserisci la lunghezza del secondo cateto: "))
+
+ipotenusa = calcola_ipotenusa(cateto1, cateto2)
+
+print("L'ipotenusa del triangolo rettangolo è:" ipotenusa)
+
+# nessuna soluzione funziona
+                                           
+
