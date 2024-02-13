2444e6cd297dcd8aaa28b84f2f0ee28ac3774b34
elmoriconi <159887303+elmoriconi@users.noreply.github.com>
2024-02-13 20:43:47 +0100

    2nd update secondo.py

--- secondo.py
+++ secondo.py
@@ -2,3 +2,11 @@ from math import sqrt
 
 print("ipotenusa = " sqrt((10,123 ** 2) + (30,456 ** ")))
                                            
+# in alternativa ho provato
+
+float c = 0
+a = 10,123 ** 2
+b = 30,456 ** 2
+c = a + b
+
+print(sqrt(c))
