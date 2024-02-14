71f46d79bdd036ec98604169ccb3f51f2db304bf
elmoriconi <ele.9.9@virgilio.it>
2024-02-14 15:37:36 +0100

    last update

--- secondo.py
+++ secondo.py
@@ -1,8 +1,8 @@
 import math
-
-cateto1 = 10123
-cateto2 = 30.456
-
-ipotenusa = math.sqrt(cateto1**2 + cateto2**2)
+                                           
+a = 10.123 ** 2
+b = 30.456 ** 2
+c = a + b
+ipotenusa = math.sqrt(c)
 
 print(ipotenusa)
