5e0f389ab71a5c0b9e1793e0d08ac118e93b60de
elmoriconi <ele.9.9@virgilio.it>
2024-02-14 18:37:14 +0100

    boh

--- secondo.py
+++ secondo.py
@@ -1,12 +1,55 @@
-import math 
-                                        
-a = 10.123 ** 2
-b = 30.456 ** 2
-c = a + b
-ipotenusa = math.sqrt(c)
+"""definire: 
+    due variabili di tipo intero, 
+    due variabili di tipo stringa, 
+    due variabili di tipo float, 
+    due variabili di tipo booleano 
+stampare:
+    i1 + i2 
+    s1 + s2 
+    f1 + f2 
+    b1 + b2 
+    i1 + s1
+    s1 + i2
+    i1 + f2
+    f2 + i2
+    i1 + b1
+    f1 + b1
+    s1 + b1
+    b1 + i1
+    b1 + s1
+    b1 + f1
 
-print(ipotenusa)
+e commentare i risultati """
+
+i1 = 2
+i2 = 5
+s1 = "Ciao"
+s2 = "Hello"
+f1 = 10.2
+f2 = 0.4
+b1 = True
+b2 = False
+
+a = i1 + i2 
+b = s1 + s2 
+c = f1 + f2 
+d = b1 + b2 
+#e = i1 + s1 somma tra un intero e una stringa, non si può fare
+#f = s1 + i2 somma tra intero e stringa, errore
+g = i1 + f2 #somma tra intero e float
+h = f2 + i2 #somma tra intero e float
+i = i1 + b1 #somma tra intero e booleano, se il booleano è true sommo 1 altrimento sommo 0
+j = f1 + b1 #somma tra float e booleano, stesso di sopra
+#k = s1 + b1 somma tra stringa e booleano, non si può
+l = b1 + i1 #somma tra booleano e intero 
+#m = b1 + s1 #somma tra booleano e stringa, non si può
+n = b1 + f1 #somma tra booleano e float
+
+print(a, b, c, d)
+print(g, h, i, j)
+print(l, n)
 
 
-                                                                                  
-
+"""
+print e input
+"""
---
+++ terzo.py
@@ -0,0 +1,12 @@
+pippo = 10
+8 = 30 #questo è sbagliato, perché 8 vale 8
+"pluto" = 20 #questo è sbagliato, perché pluto è una stringa e quindi vale se stessa
+paperino = "30" #paperino è un simbolo che vale stringa 30
+ziopaperone = "true" #uguale a riga 4
+gastone = False #gastone è un simbolo che vale un valore booleano
+peppe = 'a' #uguale a riga 4 e 5, in python anche il singolo apice delimita stringa
+numero = 100010.2
+numerone = 1191..2..3..4 #è sbagliato perché non è un numero
+numerone2 = 1191.+.2.+.3.+.4 #è giusto perché è una espressione 
+print(10/3) #stampa 3,3 periodico
+print(int(10/3)) #stampa solo la parte intera della divisione
