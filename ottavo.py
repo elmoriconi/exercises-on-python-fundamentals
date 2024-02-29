c76e343f706d3cdee0e7cc06505168f38859c7c0
elmoriconi <ele.9.9@virgilio.it>
2024-02-29 18:17:22 +0100

    nuovo es

---
+++ ottavo.py
@@ -0,0 +1,16 @@
+#Sapendo che la funzione random.randint(start, end) genera un numero intero
+#compreso tra start e end, end compreso, costruire una lista di numeri casuali 
+#lunga 100 e stampare la somma di tutti i suoi numeri
+    
+import random   
+
+lista = []
+for i in range(101):
+    x=random.randint(1, 10)  
+    lista.append(x)
+
+somma = 0
+for i in lista:
+    somma = somma + i
+
+print(somma)
