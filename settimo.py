45bbaabb768845caa7543631949b169f5a1875c9
elmoriconi <ele.9.9@virgilio.it>
2024-02-29 18:01:04 +0100

    agg

--- settimo.py
+++ settimo.py
@@ -2,3 +2,20 @@
 
 for i in range(23, 231, 23):
     print(i)
+
+#oppure
+
+for i in range(10):
+    print(23 * (i + 1))
+
+#oppure
+
+for i in range(1, 11):
+    print(23 * i)
+
+t = 23
+for i in range(10):
+    print(t)
+    t = t + 23
+
+#ce ne sono tanti altri   
