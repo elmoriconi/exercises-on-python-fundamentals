c97a3e684f73b09deb963db25ad05b781c087162
elmoriconi <ele.9.9@virgilio.it>
2024-02-29 18:16:23 +0100

    un es solo

--- settimo.py
+++ settimo.py
@@ -2,7 +2,7 @@
 
 for i in range(23, 231, 23):
     print(i)
-
+"""
 #oppure
 
 for i in range(10):
@@ -20,19 +20,4 @@ for i in range(10):
 
 #ce ne sono tanti altri
 
-#Sapendo che la funzione random.randint(start, end) genera un numero intero
-#compreso tra start e end, end compreso, costruire una lista di numeri casuali 
-#lunga 100 e stampare la somma di tutti i suoi numeri
-    
-import random   
-
-lista = []
-for i in range(101):
-    x=random.randint(1, 10)  
-    lista.append(x)
-
-somma = 0
-for i in lista:
-    somma = somma + i
-
-print(somma)
+"""
