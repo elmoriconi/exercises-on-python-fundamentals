223d58429d2b87a397f546df818f342518e2fe6f
elmoriconi <ele.9.9@virgilio.it>
2024-03-07 15:46:32 +0100

    modifica

--- decimo.py
+++ decimo.py
@@ -3,17 +3,17 @@ import time
 
 start = time.time_ns()
 lista = []
-for v in range(1, 10000000):
-    lista.append(random.randint(1, 1000000000))
+for v in range(1, 10):
+    lista.append(random.randint(1, 10))
 end = time.time_ns()
 
 print(f"Il tempo richiesto è: {(end-start)/10000000}")
 
 start = time.time_ns()
 trovati = 0
-for v in range(1, 10000000):
+for v in range(1, 10):
     if random.randint(1, 1000000000) in lista:
         trovati += 1
 end = time.time_ns()
 
-print(f"Il tempo richiesto per cercare è: {(end-start)/10000000} e ne hai trovati {trovati}")
+print(f"Il tempo richiesto per cercare è: {(end-start)/10} e ne hai trovati {trovati}")
