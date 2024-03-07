5d6fa03492ee00728aa6ee556ef959e143fc7ea1
elmoriconi <ele.9.9@virgilio.it>
2024-03-07 15:36:49 +0100

    es di oggi

---
+++ decimo.py
@@ -0,0 +1,19 @@
+import random
+import time
+
+start = time.time_ns()
+lista = []
+for v in range(1, 10000000):
+    lista.append(random.randint(1, 1000000000))
+end = time.time_ns()
+
+print(f"Il tempo richiesto è: {(end-start)/10000000}")
+
+start = time.time_ns()
+trovati = 0
+for v in range(1, 10000000):
+    if random.randint(1, 1000000000) in lista:
+        trovati += 1
+end = time.time_ns()
+
+print(f"Il tempo richiesto per cercare è: {(end-start)/10000000} e ne hai trovati {trovati}")
