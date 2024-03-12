0e9c361fb50158b13319530f7732e21ef471e8f9
elmoriconi <ele.9.9@virgilio.it>
2024-03-12 10:45:02 +0100

    aggiornato

--- tredicesimo.py
+++ tredicesimo.py
@@ -6,3 +6,11 @@ if a % 2 == 0:
     print(b+c)
 else:
     print(b-c)
+
+#si poteva scrivere anche con gli operatori booleani: 
+#if a & 1 == 0:
+
+#terzo modo:
+# if math.floor(a/2)*2 == a
+#che significa? esempio: 7/2 = 3,5 => prendo solo la parte intera
+# 3 * 2 = 6. 6 == 7 ? no, quindi dispari
