548d80912e38165629519fef5f76e23c5dbcc81d
elmoriconi <ele.9.9@virgilio.it>
2024-03-14 16:39:00 +0100

    agg

--- quindicesimo.py
+++ quindicesimo.py
@@ -6,3 +6,18 @@ def StringToList(a):
 
 a = "123"
 print(StringToList(a))
+
+######################################################################à
+
+
+fin = open("alice.txt", "r")    # = apri il file che si chiama "...", "r" significa IN LETTURA. 
+                                #Un file si può appendere in lettura, scrittura o per appendere ("w")
+                                #fin è un boh
+linee = fin.readlines()         #linee è una lista di stringhe
+fin.close()
+#readlines legge tutte le righe incluso il carattere a capo \n (eol/eoln). Per levare \n faccio 'stripping':
+l1 = []
+for linea in linee:
+    l1.append(linea.strip())
+linee = l1
+print(linee)
