d04afa98f7b77affc2ccd39a5464a96ca1a02aa5
elmoriconi <ele.9.9@virgilio.it>
2024-03-14 15:49:07 +0100

    ultimo

--- quattordicesimo.py
+++ quattordicesimo.py
@@ -99,13 +99,3 @@ def Combinazioni():
     return digits
 
 print(Combinazioni())
-
-
-# Esercizio: ho la stringa "123", la voglio trasformare in [1, 2, 3].
-#definire una funzione che risolva il problema
-
-def StringToList(a):
-    return [int(x) for x in a]
-
-a = "123"
-print(StringToList(a))
