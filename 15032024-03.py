9a07dcb6333b2b50334dae2a532a664406d1abff
elmoriconi <159887303+elmoriconi@users.noreply.github.com>
2024-03-16 19:59:52 +0100

    Update da casa sabato

--- 15032024-03.py
+++ 15032024-03.py
@@ -5,29 +5,148 @@ filejson = open("all-world-cup-players.json", "r")
 worldcup = json.load(filejson)
 filejson.close()
 
+"""
 print(len(worldcup))
 
 print(worldcup[1200])
 print(worldcup[1200]['DateOfBirth'])
 
+"""
 
 # !!!! SONO TUTTE OPERAZIONI SU LISTE !!!!
+
 # 1) Contare quanti calciatori hanno giocato per l'Italia
+
+filejson = open("all-world-cup-players.json", "r")
+worldcup = json.load(filejson)
+filejson.close()
+calcita = 0
+for i in worldcup:
+    if i["Team"] == "ITA":
+        calcita += 1
+    elif i["Team"] == "Italy":
+        calcita += 1
+    else:
+        continue
+print("Calciatori italiani: ", calcita)
+
 # 2) Contare quanti calciatori hanno giocato per il Brasile
+
+filejson = open("all-world-cup-players.json", "r")
+worldcup = json.load(filejson)
+filejson.close()
+calcbra = 0
+for i in worldcup:
+    if i["Team"] == "BRA":
+        calcbra += 1
+    elif i["Team"] == "Brazil":
+        calcbra += 1
+    else:
+        continue
+print("Calciatori brasiliani: ", calcbra)
+
 # 3) Contare quanti calciatori hanno giocato per l'Argentina
+
+filejson = open("all-world-cup-players.json", "r")
+worldcup = json.load(filejson)
+filejson.close()
+calcarg = 0
+for i in worldcup:
+    if i["Team"] == "ARG":
+        calcarg += 1
+    elif i["Team"] == "Argentina":
+        calcarg += 1
+    else:
+        continue
+print("Calciatori argentini: ", calcarg)
+
 # 4) Indicare quali calciatori hanno giocato sia per il Brasile, sia per l'Italia
+
+filejson = open("all-world-cup-players.json", "r")
+worldcup = json.load(filejson)
+filejson.close()
+calcbra = set()
+calcita = set()
+for i in worldcup:
+    if i["Team"] == "Brazil":
+        x = i.get("FullName")
+        calcbra.add(x)
+    elif i["Team"] == "BRA":
+        x = i.get("FullName")
+        calcbra.add(x)
+for i in worldcup:
+    if i["Team"] == "Italy":
+        x = i.get("FullName")
+        calcita.add(x)
+    elif i["Team"] == "ITA":
+        x = i.get("FullName")
+        calcita.add(x)
+calcbra.intersection_update(calcita)
+print("I giocatori che hanno giocato sia per il Brasile che per l'Italia sono: ", calcbra)
+
 # 5) Indicare quali calciatori hanno giocato sia per l'Argentina, sia per l'Italia
+
+filejson = open("all-world-cup-players.json", "r")
+worldcup = json.load(filejson)
+filejson.close()
+calcarg = set()
+calcita = set()
+for i in worldcup:
+    if i["Team"] == "Argentina":
+        x = i.get("FullName")
+        calcarg.add(x)
+    elif i["Team"] == "ARG":
+        x = i.get("FullName")
+        calcarg.add(x)
+for i in worldcup:
+    if i["Team"] == "Italy":
+        x = i.get("FullName")
+        calcita.add(x)
+    elif i["Team"] == "ITA":
+        x = i.get("FullName")
+        calcita.add(x)
+calcarg.intersection_update(calcita)
+print("I giocatori che hanno giocato sia per l'Argentina che per l'Italia sono: ", calcarg)
+
+
 # 6) Trovare qual'è il calciatore più giovane che ha partecipato alla coppa del mondo
+
+
+
 # 7) Trovare qual'è il calciatore più anziano che ha partecipato alla coppa del mondo
+
+
+
 # 8) Trovare quale calciatore ha partecipato a più edizioni della coppa del mondo
-# 9) Trovare quale squadra di calcio ha fornito più calciatori per la coppa del mondo
-#    Organizzare per nazione. Cioè quale squadra italiana ha fornito più calciatori per la coppa del mondo e quanti, quale squadra francese, ...
+
+filejson = open("all-world-cup-players.json", "r")
+worldcup = json.load(filejson)
+filejson.close()
+lista = []            #inserisco tutti i nomi in una lista 
+for i in worldcup:
+    x = i.get("FullName")
+    lista.append(x)
+diz = {c: lista.count(c) for c in lista}            #conto quante volte si ripete ogni nome
+calciatore = " "
+partecipazioni = 1
+for a in diz:
+    y = a.get(a)
+    if y > partecipazioni:
+        calciatore = a
+    else:
+        continue
+print(calciatore)
+
+# 9) Trovare quale squadra di calcio ha fornito più calciatori per la coppa del mondo. Organizzare per nazione. 
+#    Cioè quale squadra italiana ha fornito più calciatori per la coppa del mondo e quanti, quale squadra francese, ...
+
+
 
 #Conta quanti calciatori per ogni squadra. La key è team e il value è il numero di calciatori 
-quanticalciatori = dict()
-for v in worldcup:
-    # se i["Team"] è già presente, sommo 1, altrimenti metto a 1
-    if v["Team"] in quanticalciatori.keys():
-        quanticalciatori[v["Team"]] = quanticalciatori[v["Team"]]+1
-    else:
-        quanticalciatori[v["Team"]] = 1
+#quanticalciatori = dict()
+#for v in worldcup:
+    # se v["Team"] è già presente, sommo 1, altrimenti metto a 1
+#    if v["Team"] in quanticalciatori.keys():
+#        quanticalciatori[v["Team"]] = quanticalciatori[v["Team"]]+1
+#    else:
+#        quanticalciatori[v["Team"]] = 1
