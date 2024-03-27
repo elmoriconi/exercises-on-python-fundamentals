86ca77c37d834aa0656469075bcecf219bd9fb10
elmoriconi <ele.9.9@virgilio.it>
2024-03-27 18:56:03 +0100

    es da finire

--- ventesimo.py
+++ ventesimo.py
@@ -1,6 +1,4 @@
-
-
-def MCD(a, b):
+"""def MCD(a, b):
     while a != b:
         if a > b:
             a = a - b
@@ -11,3 +9,18 @@ def MCD(a, b):
 a = int(input("Inserire il primo numero: "))
 b = int(input("Inserire il secondo numero: "))
 print("MCD: ", MCD(a, b))
+"""
+def CancMultipli(n, lista):
+    for i in range(10):
+        m = i * n
+        lista.pop(m)
+    return lista
+
+lista = []
+primi = []
+for i in range(2, 1000000):
+    lista.append(i)
+for x in lista:
+    CancMultipli(x, lista)
+    primi.append(x)
+print(primi)
