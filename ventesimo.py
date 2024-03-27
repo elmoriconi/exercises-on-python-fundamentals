1e7c568a629636aa1f1d05c83c529ec08cc7ca0f
elmoriconi <ele.9.9@virgilio.it>
2024-03-27 17:38:20 +0100

    risposte

--- ventesimo.py
+++ ventesimo.py
@@ -6,8 +6,8 @@ def MCD(a, b):
             a = a - b
         else:  
             b = b - a    
-    return a
+    return a 
 
 a = int(input("Inserire il primo numero: "))
 b = int(input("Inserire il secondo numero: "))
-print(MCD(a, b))
+print("MCD: ", MCD(a, b))
